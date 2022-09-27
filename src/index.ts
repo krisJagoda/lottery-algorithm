c216c94f7cfcb75204722fa6d0f80ee42d7004ff
Krystian Jagoda <kriswro1989@gmail.com>
2022-09-28 01:40:11 +0200

    Improve code

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,20 @@
 import { Lottery } from "./lottery-application";
-import { writeFile } from 'fs';
-
-console.log("* Initializing Lottery Application *");
-
-var log = '***** Lottery application log *****\n\n';
+import * as fs from "fs";
+import {writeFile} from "fs";
 
 const upperBoundary = 60;
 const numbersToDraw = 6;
+const timestamp = new Date().toISOString();
+const dir = './results';
+const lottery = new Lottery(upperBoundary, numbersToDraw);
+const fileName = `${dir}/lottery-results:${timestamp}.txt`;
+
+console.log("* Initializing Lottery Application *");
+
+let log = '***** Lottery application log *****\n\n';
 
 log += 'Generating numbers from 0 to ' + upperBoundary + '\n';
 log += 'Numbers to draw: ' + numbersToDraw + '\n';
-
-const lottery = new Lottery(upperBoundary, numbersToDraw);
 
 console.log("** Running **");
 
@@ -23,8 +26,9 @@ log += '\n\n';
 log += '** Statistics **\n';
 log += lottery.getDurations();
 log += '\n\n';
-log += 'TOTAL DURATION: ' + lottery.totalDuration;
+log += `TOTAL DURATION ${lottery.totalDuration} IN MILLISECONDS.`
 
-var timestamp = Date.now();
-var filename = './results/lottery-result' + timestamp + '.txt';
-writeFile(filename, log, () => console.log('Check result file ' + filename));
+if (!fs.existsSync(dir)){
+    fs.mkdirSync(dir);
+}
+writeFile(fileName, log, () => console.log('Check result file ' + fileName));
---
+++ src/tests/unit/lottery-application.test.ts
@@ -0,0 +1,34 @@
+import {describe, expect} from '@jest/globals';
+import {Lottery} from "../../lottery-application";
+
+describe('Lottery constructor testing', () => {
+    it('should throw exception if first argument is 0 or below 0',  () => {
+        try {
+            new Lottery(0, 10);
+            new Lottery(-1, 10);
+        } catch (err) {
+            expect(err).toBeInstanceOf(Error);
+            expect(err.message).toBe('Max cannot be 0 or a negative integer.');
+        }
+    });
+
+    it('should throw exception if second argument is 0 or below 0',  () => {
+        try {
+            new Lottery(10, 0);
+            new Lottery(10, -10);
+        } catch (err) {
+            expect(err).toBeInstanceOf(Error);
+            expect(err.message).toBe('NumbersToDraw cannot be 0 or a negative integer.');
+        }
+    });
+
+    it('should throw exception second argument is bigger than first argument',  () => {
+        try {
+            new Lottery(2, 10);
+        } catch (err) {
+            expect(err).toBeInstanceOf(Error);
+            expect(err.message).toBe('Numbers to draw cannot be bigger than max numbers in lottery.');
+        }
+    });
+
+});
